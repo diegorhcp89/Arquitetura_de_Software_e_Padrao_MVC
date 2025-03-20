c63d801b1d46be916e5875cf3303139127e6b6c7
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-03-20 17:03:49 -0300

    Implementando Delete Pets

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -10,3 +10,10 @@ def test_list_pets():
     response = repo.list_pets()
     print()
     print(response)
+
+def test_delete_pet():
+    name = "belinha"
+
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
+    
